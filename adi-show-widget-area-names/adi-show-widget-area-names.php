0491f5bc1ade26413e92b972242549fbb3e6e18f
kmfj1 <kmfj1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-12 19:54:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/show-widget-area-names/trunk@611677 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adi-show-widget-area-names/adi-show-widget-area-names.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: ADI Show Widget Area Names
+Plugin URI: http://www.amplitudedesign.com
+Description: Plugin to show the widget area names on non-admin pages and posts. This adds the title to widget $params['before_widget']. This only display for the edit_posts capabilities. You must use print $params['before_widget'] in your widget definitions to view the area name.
+Version: 1.0.1
+Author: Kevin Johnson
+Author URI: http://www.amplitudedesign.com
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/**
+ * This adds the name of the area to the 'before_widget'
+ * @param array $params
+ */
+class ShowWidgetTitle{
+
+function widget_show_title($params){
+	// If the user can't manage options return $name	
+	if (!current_user_can('edit_posts') )return($params);
+	$params[0]['before_widget'] .= '<div class="adi-widget-area-name">'.$params[0]['name'].'&nbsp;<a target="_blank" href="'.site_url().'/wp-admin/widgets.php" title="'.$params[0]['name'].'" alt="'.$params[0]['name'].'">[edit]</a></div>';
+	return($params);
+}
+
+function widget_show_title_load(){
+//Only register on the non-admin pages of the site.
+	if(current_user_can('edit_posts') && !is_admin()){
+		add_filter("dynamic_sidebar_params",array(__CLASS__,'widget_show_title'));
+	}
+}
+
+function widget_show_title_scripts(){
+	if(current_user_can('edit_posts') && !is_admin()){
+		wp_register_style('adi-widget-area-names',plugins_url().'/adi-show-widget-area-names/css/adi-show-widget-areas.css');
+		wp_enqueue_style('adi-widget-area-names');
+	}
+}
+
+}
+if(!is_admin())add_action('plugins_loaded',array('ShowWidgetTitle','widget_show_title_load'));
+if(!is_admin())add_action('wp_enqueue_scripts',array('ShowWidgetTitle','widget_show_title_scripts'));
