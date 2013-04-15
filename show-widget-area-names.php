36502fad592791abb277f45827bc810c97000610
kmfj1 <kmfj1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-15 01:58:02 +0000

    Updated version number.
    git-svn-id: https://plugins.svn.wordpress.org/show-widget-area-names/tags/1.0.3@697546 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- show-widget-area-names.php
+++ show-widget-area-names.php
@@ -3,7 +3,7 @@
 Plugin Name: Show Widget Area Names
 Plugin URI: http://www.amplitudedesign.com
 Description: Plugin to show the widget area names on non-admin pages and posts. This adds the title to widget $params['before_widget']. 
-Version: 1.0.1
+Version: 1.0.4
 Author: Kevin Johnson
 Author URI: http://www.amplitudedesign.com
 License: GPLv2 or later
