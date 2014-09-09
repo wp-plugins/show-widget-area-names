4785380c3e6c1e3c2cbc8761b2f2923f8af31451
kmfj1 <kmfj1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-09 17:20:16 +0000

    Changes to __CLASS__ reference. Tested on 4.0.0
    git-svn-id: https://plugins.svn.wordpress.org/show-widget-area-names/trunk@985546 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: kmfj1
 Tags: widgets
 Requires at least: 3.4.1
-Tested up to: 3.5.1
-Stable tag: 1.0.7
+Tested up to: 4.0.0
+Stable tag: 1.0.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- show-widget-area-names.php
+++ show-widget-area-names.php
@@ -3,7 +3,7 @@
 Plugin Name: Show Widget Area Names (SWAN)
 Plugin URI: http://www.amplitudedesign.com
 Description: SWAN displays the widget area names on non-admin pages and posts. This adds the title to widget $params['before_widget']. 
-Version: 1.0.6
+Version: 1.0.8
 Author: Kevin Johnson
 Author URI: http://www.amplitudedesign.com
 License: GPLv2 or later
@@ -42,7 +42,7 @@ function widget_show_title($params){
 function widget_show_title_load(){
 //Only register on the non-admin pages of the site.
 	if(current_user_can('edit_posts') && !is_admin()){
-		add_filter("dynamic_sidebar_params",array(__CLASS__,'widget_show_title'));
+		add_filter("dynamic_sidebar_params",array('ShowWidgetTitle', 'widget_show_title'));
 	}
 }
 
